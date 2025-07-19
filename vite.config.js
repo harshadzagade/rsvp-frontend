ce8b085d8e7d6dab9450a480324c3ada73237273
harshadzagade <hzagade21@gmail.com>
2025-07-19 16:49:49 +0530

    Update API endpoints to use production URL

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5350',
+        target: 'https://fdp.met.edu',
         changeOrigin: true,
         secure: false,
       },
